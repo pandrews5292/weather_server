8ab51f4975d3ca933fb69bc7aa9a16187e2b1a3c
cookiesup <mmelchor@fandm.edu>
2014-02-25 20:15:04 -0800

    functions added

--- weather.rb
+++ weather.rb
@@ -1,5 +1,6 @@
-require 'sinatra'
-require 'socket'
+require 'net/http'
+require 'rubygems'
+require 'json'
 
 class Weather
   #Class that will return weather info
@@ -16,18 +17,42 @@ class Weather
     return @zip
   end
   
-  def FullWeather()
-    data = Net::HTTP.get('api.openweather.org','/data/2.5/weather?q=')
+  def getTemp()
+    data = fullWeather
+    parsed = JSON.parse(data)
+    t =  parsed["main"]["temp"]
+    return t
+  end
+    
+  def getHumidity()
+    data = fullWeather
+    parsed = JSON.parse(data)
+    t = parsed["main"]["humidity"]
+    return t
+  end
+  
+  def getWind()
+    data = fullWeather
+    parsed = JSON.parse(data)
+    t = parsed["wind"]["speed"]
+    return t
 
+  end
 
-
-
-
-
-
-
-
-
-
-
+  def fullWeather()
+    data = Net::HTTP.get('api.openweathermap.org','/data/2.5/weather?q=Lancaster')
+    return data
+  end
 end
+
+w = Weather.new()
+#puts w.fullWeather
+print "Temperature: ", w.getTemp,"\n"
+print "Humidity: ", w.getHumidity,"\n"
+print "Wind: ",w.getWind,"\n"
+
+
+
+
+
+
