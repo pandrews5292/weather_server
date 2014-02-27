8470fabe582e69495a975b4038f4127b4119fa66
Patrick Andrews <pandrews@Patricks-MacBook-Pro.local>
2014-02-26 21:20:47 -0500

    added some bootstrap stuff

--- api/weather.rb
+++ api/weather.rb
@@ -3,7 +3,7 @@ require 'rubygems'
 require 'json'
 
 class Weather
-  #Class that will return weather info
+  #Weather information
   def initialize()
     @city = 'Lancaster'
     @zip = '17603'
@@ -21,7 +21,8 @@ class Weather
     data = fullWeather
     parsed = JSON.parse(data)
     t =  parsed["main"]["temp"]
-    return t
+    f = (9/5)*(t-273) + 32
+    return f.round(1)
   end
     
   def getHumidity()
@@ -38,18 +39,33 @@ class Weather
     return t
 
   end
+  
+  def getDescription()
+    data = fullWeather
+    parsed = JSON.parse(data)
+    t = parsed["weather"][0]["description"]
+    return t
+  end
 
   def fullWeather()
     data = Net::HTTP.get('api.openweathermap.org','/data/2.5/weather?q=Lancaster')
     return data
   end
+
+  def displayAll()
+    print "City: ",self.getCity,"\n"
+    print "Zip Code: ",self.getZip,"\n"
+    print "Temperature: ", self.getTemp," degrees F\n"
+    print "Humidity: ", self.getHumidity,"%\n"
+    print "Wind: ",self.getWind," mph\n"
+    print "Looks like: ",self.getDescription,"\n"
+  end
 end
 
-w = Weather.new()
-#puts w.fullWeather
-print "Temperature: ", w.getTemp,"\n"
-print "Humidity: ", w.getHumidity,"\n"
-print "Wind: ",w.getWind,"\n"
+#w = Weather.new()
+#w.displayAll
+
+
 
 
 
